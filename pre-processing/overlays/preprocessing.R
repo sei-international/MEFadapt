cdf03b68f17a80c6b7f56285e6d5e4d229ee5dfa
Rafaela Flach <rafaela.a.flach@gmail.com>
2025-07-29 10:27:15 +0200

    Update preprocessing.R

--- pre-processing/overlays/preprocessing.R
+++ pre-processing/overlays/preprocessing.R
@@ -7,11 +7,7 @@ libraries <- c(
     "aws.s3", "lubridate", "tibble", "stringi",
     "aws.signature", "ggpubr", "stringr", "janitor",
     "magrittr", "tidyverse", "dplyr", "sf", "raster",
-<<<<<<< Updated upstream
     "terra"
-=======
-    "fasterize"
->>>>>>> Stashed changes
 )
 
 lapply(libraries, library, character.only = TRUE)
@@ -26,7 +22,6 @@ Sys.setenv("AWS_DEFAULT_REGION" = get_location("mefadapt"))
 
 # aqueduct
 
-<<<<<<< Updated upstream
 future_annual <- s3read_using(st_read,
     object = "repository/raw_data/WATER/aqueduct-4-0-water-risk-data/Aqueduct40_waterrisk_download_Y2023M07D05/aqueduct_future_annual.gpkg",
     bucket = bucket
@@ -48,36 +43,6 @@ f_annual <- terra::rasterize(test, ras, "bau50_ws_x_l", background = NA)
 
 s3write_using(f_annual, terra::writeRaster,
     object = "repository/raw_data/WATER/aqueduct-4-0-water-risk-data/Aqueduct40_waterrisk_download_Y2023M07D05/GDB/Aq40_Y2023D07M05_bau50_ws_x_l.tif",
-=======
-future_annual <- get_bucket("repository/raw_data/WATER/aqueduct-4-0-water-risk-data/Aqueduct40_waterrisk_download_Y2023M07D05/GDB/Aq40_Y2023D07M05.gdb/",
-    bucket = bucket
-) %>% map(~ .x$Key)
-
-future_annual <- get_bucket("repository/raw_data/WATER/aqueduct-4-0-water-risk-data/",
-    bucket = bucket
-) %>% map(~ .x$Key)
-
-tmpdir <- "C:/Users/RafaelaFlach/Downloads/tempdir/"
-
-subdir <- "Aq40_Y2023D07M05.gdb/"
-dir.create(file.path(tmpdir, subdir), showWarnings = FALSE)
-
-lapply(future_annual[-1], function(x) {
-    save_object(object = x, bucket = bucket, file = paste0(tmpdir, subdir, basename(x)))
-})
-
-future_annual <- st_read(paste0(tmpdir, subdir), layer = "future_annual")
-baseline_annual <- st_read(paste0(tmpdir, subdir), layer = "baseline_annual")
-
-s3write_using(future_annual, st_write,
-    object = "repository/raw_data/WATER/aqueduct-4-0-water-risk-data/Aqueduct40_waterrisk_download_Y2023M07D05/GDB/Aq40_Y2023D07M05_future.gpkg",
-    bucket = bucket
-)
-
-
-s3write_using(baseline_annual, st_write,
-    object = "repository/raw_data/WATER/aqueduct-4-0-water-risk-data/Aqueduct40_waterrisk_download_Y2023M07D05/GDB/Aq40_Y2023D07M05_baseline.gpkg",
->>>>>>> Stashed changes
     bucket = bucket
 )
 
